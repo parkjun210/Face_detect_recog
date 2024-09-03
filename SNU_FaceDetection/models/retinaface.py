4840917d57bc23c1b21312e7ed32942948f6fd82
seonjipark <seonji2805@google.com>
2024-09-03 13:59:29 +0900

    update SNU_FaceDetection

--- SNU_FaceDetection/models/retinaface.py
+++ SNU_FaceDetection/models/retinaface.py
@@ -45,10 +45,22 @@ class LandmarkHead(nn.Module):
         out = out.permute(0, 2, 3, 1).contiguous()
 
         return out.view(out.shape[0], -1, 10)
+    
+class IoUHead(nn.Module):
+    def __init__(self, inchannels=512, num_anchors=3):
+        super(IoUHead, self).__init__()
+        self.conv1x1 = nn.Conv2d(inchannels, num_anchors * 1, kernel_size=(1, 1), stride=1, padding=0)
+
+
+    def forward(self, x):
+        out = self.conv1x1(x)
+        out = out.permute(0, 2, 3, 1).contiguous()
+
+        return out.view(out.shape[0], -1, 1)
 
 
 class RetinaFace(nn.Module):
-    def __init__(self, phase='train'):
+    def __init__(self, phase='train', version='retina', anchor_num=3):
         """
         :param cfg:  Network related settings.
         :param phase: train or test.
@@ -56,14 +68,16 @@ class RetinaFace(nn.Module):
         super(RetinaFace, self).__init__()
         self.phase = phase
         self.fpn_num = 3
-        self.anchor_num = 2
+        self.anchor_num = anchor_num
         self.network = 'resnet50'
+        self.model_version = version
 
         backbone = None
 
 
         import torchvision.models as models
-        backbone = models.resnet50(weights='ResNet50_Weights.DEFAULT')
+        # backbone = models.resnet50(weights='ResNet50_Weights.DEFAULT')
+        backbone = models.resnet50(pretrained=True)
 
         in_channel = 256
         out_channel = 256
@@ -89,6 +103,8 @@ class RetinaFace(nn.Module):
         self.ClassHead_1 = self._make_class_head(fpn_num=self.fpn_num, inchannels=out_channel, anchor_num=self.anchor_num)
         self.BboxHead_1 = self._make_bbox_head(fpn_num=self.fpn_num, inchannels=out_channel, anchor_num=self.anchor_num)
         self.LandmarkHead_1 = self._make_landmark_head(fpn_num=self.fpn_num, inchannels=out_channel, anchor_num=self.anchor_num)
+        if version == 'tina':
+            self.IoUHead_1 = self._make_iou_head(fpn_num=self.fpn_num, inchannels=out_channel, anchor_num=self.anchor_num)
 
         self.ssh2_1 = SSH(out_channels, out_channels)
         self.ssh2_2 = SSH(out_channels, out_channels)
@@ -97,6 +113,8 @@ class RetinaFace(nn.Module):
         self.ClassHead_2 = self._make_class_head(fpn_num=self.fpn_num, inchannels=out_channel, anchor_num=self.anchor_num)
         self.BboxHead_2 = self._make_bbox_head(fpn_num=self.fpn_num, inchannels=out_channel, anchor_num=self.anchor_num)
         self.LandmarkHead_2 = self._make_landmark_head(fpn_num=self.fpn_num, inchannels=out_channel, anchor_num=self.anchor_num)
+        if version == 'tina':
+            self.IoUHead_2 = self._make_iou_head(fpn_num=self.fpn_num, inchannels=out_channel, anchor_num=self.anchor_num) # old
 
     def _make_class_head(self, fpn_num=3, inchannels=64, anchor_num=2):
         classhead = nn.ModuleList()
@@ -115,6 +133,12 @@ class RetinaFace(nn.Module):
         for i in range(fpn_num):
             landmarkhead.append(LandmarkHead(inchannels, anchor_num))
         return landmarkhead
+    
+    def _make_iou_head(self, fpn_num=3, inchannels=64, anchor_num=2):
+        iouhead = nn.ModuleList()
+        for i in range(fpn_num):
+            iouhead.append(IoUHead(inchannels, anchor_num))
+        return iouhead
 
     def forward(self, inputs):
         out = self.body(inputs)
@@ -128,29 +152,40 @@ class RetinaFace(nn.Module):
         feature1_3 = self.ssh1_3(fpn[2])
         features_1 = [feature1_1, feature1_2, feature1_3]
 
-        bbox_regressions_1 = torch.cat([self.BboxHead_1[i](feature) for i, feature in enumerate(features_1)], dim=1)
         classifications_1 = torch.cat([self.ClassHead_1[i](feature) for i, feature in enumerate(features_1)], dim=1)
         ldm_regressions_1 = torch.cat([self.LandmarkHead_1[i](feature) for i, feature in enumerate(features_1)], dim=1)
+        
+        if self.model_version == 'tina':
+            bbox_regressions_1 = torch.cat([self.BboxHead_1[i](feature) for i, feature in enumerate(features_1)], dim=1)
+            iou_regressions_1 = torch.cat([self.IoUHead_1[i](feature) for i, feature in enumerate(features_1)], dim=1)
+        else:
+            iou_regressions_1 = None
+            bbox_regressions_1 = torch.cat([self.BboxHead_1[i](feature) for i, feature in enumerate(features_1)], dim=1)
 
         # Define context head module 2
         feature2_1 = self.ssh2_1(fpn[0])
         feature2_2 = self.ssh2_2(fpn[1])
         feature2_3 = self.ssh2_3(fpn[2])
         features_2 = [feature2_1, feature2_2, feature2_3]
-
-        bbox_regressions_2 = torch.cat([self.BboxHead_2[i](feature) for i, feature in enumerate(features_2)], dim=1)
+            
         classifications_2 = torch.cat([self.ClassHead_2[i](feature) for i, feature in enumerate(features_2)], dim=1)
         ldm_regressions_2 = torch.cat([self.LandmarkHead_2[i](feature) for i, feature in enumerate(features_2)], dim=1)
+        if self.model_version == 'tina':
+            bbox_regressions_2 = torch.cat([self.BboxHead_2[i](feature) for i, feature in enumerate(features_2)], dim=1) # old
+            iou_regressions_2 = torch.cat([self.IoUHead_2[i](feature) for i, feature in enumerate(features_2)], dim=1)
+        else:
+            iou_regressions_2 = None
+            bbox_regressions_2 = torch.cat([self.BboxHead_2[i](feature) for i, feature in enumerate(features_2)], dim=1)
 
         bbox_regressions_2 = bbox_regressions_1 + bbox_regressions_2
 
-
         if self.phase == 'train':
-            output = (bbox_regressions_1, classifications_1, ldm_regressions_1)
-            output2 = (bbox_regressions_2, classifications_2, ldm_regressions_2)
+            output = (bbox_regressions_1, classifications_1, ldm_regressions_1, iou_regressions_1)
+            output2 = (bbox_regressions_2, classifications_2, ldm_regressions_2, iou_regressions_2)
         else:
-            output = (bbox_regressions_1, F.softmax(classifications_1, dim=-1), ldm_regressions_1)
-            output2 = (bbox_regressions_2, F.softmax(classifications_2, dim=-1), ldm_regressions_2)
+            output = (bbox_regressions_1, F.softmax(classifications_1, dim=-1), ldm_regressions_1, iou_regressions_1) 
+            output2 = (bbox_regressions_2, F.softmax(classifications_2, dim=-1), ldm_regressions_2, iou_regressions_2)
+
 
         # shape of output will be (batch, 16800, 2/4/10) when anchor=2, fpn=3
         return output, output2
